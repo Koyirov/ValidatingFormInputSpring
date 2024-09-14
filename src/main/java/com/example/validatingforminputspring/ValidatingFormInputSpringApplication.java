4aa8809bd43e9240d9beed330428b05c6a4ffeb3
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2024-09-14 09:11:40 +0200

    first commit

---
+++ src/main/java/com/example/validatingforminputspring/ValidatingFormInputSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.validatingforminputspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidatingFormInputSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidatingFormInputSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/validatingforminputspring/ValidatingFormInputSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.validatingforminputspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidatingFormInputSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
