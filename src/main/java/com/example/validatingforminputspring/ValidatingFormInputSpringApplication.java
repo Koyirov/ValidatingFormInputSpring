5e0b270aa1d06285af1a7cc27e7a7b557e4052a7
Shohrukh Koyirov <shohrukhkoyirov@gmail.com>
2024-09-14 09:13:15 +0200

    syntax korrigiert

--- src/main/java/com/example/validatingforminputspring/ValidatingFormInputSpringApplication.java
+++ src/main/java/com/example/validatingforminputspring/ValidatingFormInputSpringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ValidatingFormInputSpringApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ValidatingFormInputSpringApplication.class, args);
 	}
 
